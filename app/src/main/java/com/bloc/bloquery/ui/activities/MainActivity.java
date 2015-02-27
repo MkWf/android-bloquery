2ea689ec745419f519e38465b6462501710f4ea4
Mark Wassef <markdevw@gmail.com>
2015-02-27 00:34:35 -0500

    Added question_item layout file.

--- app/src/main/java/com/bloc/bloquery/ui/activities/MainActivity.java
+++ app/src/main/java/com/bloc/bloquery/ui/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.bloc.bloquery.activities;
+package com.bloc.bloquery.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
---
+++ app/src/main/java/com/bloc/bloquery/ui/fragments/QuestionsFragment.java
@@ -0,0 +1,46 @@
+package com.bloc.bloquery.ui.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bloc.bloquery.R;
+import com.bloc.bloquery.adapters.ItemAdapter;
+
+/**
+ * Created by Mark on 2/27/2015.
+ */
+public class QuestionsFragment extends Fragment {
+
+    ItemAdapter itemAdapter;
+    RecyclerView recyclerView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        itemAdapter = new ItemAdapter();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View inflate = inflater.inflate(R.layout.fragment_questions_list, container, false);
+        recyclerView = (RecyclerView) inflate.findViewById(R.id.rv_fragment_questions);
+        return inflate;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemAdapter);
+    }
+}
