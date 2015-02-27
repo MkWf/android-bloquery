b1f57f0c11882fcf0e87b31ed6f3ed8f995e8354
Mark Wassef <markdevw@gmail.com>
2015-02-26 23:55:59 -0500

    Added Question class

---
+++ ParseLoginUI/src/com/parse/models/Question.java
@@ -0,0 +1,16 @@
+package com.parse.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+/**
+ * Created by Mark on 2/26/2015.
+ */
+
+@ParseClassName("Question")
+public class Question extends ParseObject {
+
+    public Question(){
+
+    }
+}
--- app/src/main/java/com/bloc/bloquery/BloQueryApplication.java
+++ app/src/main/java/com/bloc/bloquery/BloQueryApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.models.BloQueryUser;
+import com.parse.models.Question;
 
 /**
  * Created by Mark on 2/26/2015.
@@ -14,6 +15,7 @@ public class BloQueryApplication extends Application {
         public void onCreate(){
             super.onCreate();
             ParseObject.registerSubclass(BloQueryUser.class);
+            ParseObject.registerSubclass(Question.class);
             Parse.enableLocalDatastore(this);
             Parse.initialize(this, getResources().getString(R.string.parse_app_id), getResources().getString(R.string.parse_client_key));
         }
