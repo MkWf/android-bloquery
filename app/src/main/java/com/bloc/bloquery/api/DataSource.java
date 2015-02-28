e7017f514e62fdd4b167f451bf29bf553fee2e8f
Mark Wassef <markdevw@gmail.com>
2015-02-27 23:53:56 -0500

    Added fake questions to Parse and displayed them in QuestionsFragment

--- app/src/main/java/com/bloc/bloquery/api/DataSource.java
+++ app/src/main/java/com/bloc/bloquery/api/DataSource.java
@@ -1,5 +1,9 @@
 package com.bloc.bloquery.api;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 import com.parse.models.Question;
 
 import java.util.ArrayList;
@@ -14,9 +18,9 @@ public class DataSource {
 
     public DataSource() {
         questions = new ArrayList<Question>();
-        answers = new ArrayList<Question>();
+        //answers = new ArrayList<Question>();
         createFakeQuestions();
-        createFakeAnswers();
+        //createFakeAnswers();
     }
 
     private void createFakeAnswers() {
@@ -33,9 +37,25 @@ public class DataSource {
     }
 
     void createFakeQuestions() {
-        for(int i=0; i<10; i++){
-            questions.add(new Question(i));
-        }
+        Question p = new Question();
+        p.setQuestion("Question 1");
+        p.saveInBackground();
+
+        Question q = new Question();
+        q.setQuestion("Question 2");
+        q.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                ParseQuery<Question> query = ParseQuery.getQuery("Question");
+                query.orderByDescending("createdAt");
+                query.setLimit(10);
+                query.findInBackground(new FindCallback<Question>() {
+                    public void done(List<Question> questionsList, ParseException e) {
+                        questions = questionsList;
+                    }
+                });
+            }
+        });
     }
 }
 
